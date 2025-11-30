d3cbd3ded163193b0b69fb1af82471c0aea58dba
jdsantillan-art <jdsantillan.art@gmail.com>
2025-12-01 01:39:30 +0800

    Fix WhiteNoise static files storage for deployment

--- sirms/sirms_project/settings.py
+++ sirms/sirms_project/settings.py
@@ -99,7 +99,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # WhiteNoise configuration for static files
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
